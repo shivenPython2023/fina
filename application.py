9c1ddcfaa1786376f9aa7d5b9ce1eb5218d1e56e
shivenPython2023 <141051112+shivenPython2023@users.noreply.github.com>
2024-03-29 19:25:55 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, session, redirect, url_for, send_from_directory
 import cv2
 import numpy as np
 from PIL import Image, ImageDraw
@@ -10,7 +10,6 @@ import warnings
 from sklearn.cluster import KMeans
 import webcolors
 from werkzeug.utils import secure_filename
-from flask import session, redirect, url_for
 matplotlib.use('Agg')
 
 
@@ -21,7 +20,7 @@ matplotlib.use('Agg')
 
 
 application= Flask(__name__)
-application.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+#application.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
 @application.route('/')
