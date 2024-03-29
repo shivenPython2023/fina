ec5d7334a79dc303577b8f92a401021aa2243cd2
shivenPython2023 <141051112+shivenPython2023@users.noreply.github.com>
2024-03-29 19:47:10 -0400

    Update application.py

--- application.py
+++ application.py
@@ -3,21 +3,11 @@ from flask import Flask, render_template, request, session, redirect, url_for, s
 application= Flask(__name__)
 application.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-import os
-import cv2
-import numpy as np
-from PIL import Image, ImageDraw
-import math
-import matplotlib.pyplot as plt
-import matplotlib
-import warnings
-from sklearn.cluster import KMeans
-import webcolors
-from werkzeug.utils import secure_filename
 
 @application.route('/')
 def index():
-    return render_template('index.html')
+ return render_template('index.html')
+
 
 
 
