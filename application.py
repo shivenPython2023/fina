8217578922135296cc8c17d7b2fc01b1fa265a52
shivenPython2023 <141051112+shivenPython2023@users.noreply.github.com>
2024-03-29 19:43:41 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, session, redirect, url_for, send_from_directory
+
 application= Flask(__name__)
 application.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 import os
 import cv2
 import numpy as np
@@ -13,10 +15,9 @@ from sklearn.cluster import KMeans
 import webcolors
 from werkzeug.utils import secure_filename
 
-
 @application.route('/')
 def index():
- return render_template('index.html')
+    return render_template('index.html')
 
 
 
