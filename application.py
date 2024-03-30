ad0fe4a5d204a3ab241db00f6c23558cc2d5a985
shivenPython2023 <141051112+shivenPython2023@users.noreply.github.com>
2024-03-29 21:54:02 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,16 +1,4 @@
 from flask import Flask, render_template, session, request, redirect, url_for, send_from_directory
-import cv2
-import numpy as np
-from PIL import Image, ImageDraw
-import math
-import matplotlib.pyplot as plt
-import matplotlib
-import warnings
-from sklearn.cluster import KMeans
-import webcolors
-from werkzeug.utils import secure_filename
-import os
-
 
 application= Flask(__name__)
 application.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
